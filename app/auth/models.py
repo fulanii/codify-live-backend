a485a368ed3cdb49864e93d0982ff78561cc9fe5
Yassine Issoufou <68094236+fulanii@users.noreply.github.com>
2025-11-24 17:13:35 -0600

    added profile creations sql to auth/modesls

---
+++ app/auth/models.py
@@ -0,0 +1,10 @@
+
+
+## Profile table creation
+profiles_sql ="""
+create table profiles (
+   id uuid primary key references auth.users(id) on delete cascade,
+   username text unique not null,
+   created_at timestamp with time zone default now()
+);
+"""
